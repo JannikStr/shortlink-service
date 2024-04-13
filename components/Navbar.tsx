08e8d6fccbafef5e1305670dd723ee109041332f
Jannik Streitberg <jannik@streitberg.name>
2024-04-13 16:31:22 +0200

    Replaced profile with simple sign out

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,6 @@
 
 import { signOut, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import { FaMask } from "react-icons/fa";
-import { DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
 
 export default function Navbar() {
   const { data: session } = useSession();
@@ -12,17 +9,7 @@ export default function Navbar() {
   return (
     <nav className="w-full flex justify-end pr-3 pt-2 text-white ">
       { session ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button className="text-zinc-950" variant="outline"><FaMask /><span className="pl-2">Profile</span></Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-36">
-            <DropdownMenuLabel>Account</DropdownMenuLabel>
-            <DropdownMenuItem>
-              <Button onClick={() => signOut()}>Log Out</Button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Button onClick={() => signOut()} variant="destructive">Sign Out</Button>
       ) : (
         <></>
       )}
