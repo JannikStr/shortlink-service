fea2a777ebcbf5e4373d0cd36e9465699c19f09b
Jannik Streitberg <jannik@streitberg.name>
2024-04-13 16:44:18 +0200

    Add "copy to clipboard" button

--- components/LinkTableRow.tsx
+++ components/LinkTableRow.tsx
@@ -2,8 +2,9 @@ import { LinkDocument } from "@/models/Link";
 import { TableCell, TableRow } from "./ui/table";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { FaPencilAlt, FaRegTrashAlt } from "react-icons/fa";
+import { FaCopy, FaPencilAlt, FaRegTrashAlt } from "react-icons/fa";
 import { Skeleton } from "./ui/skeleton";
+import { useToast } from "./ui/use-toast";
 
 interface LinkTableRowProps {
   shortlink: LinkDocument|undefined;
@@ -13,7 +14,14 @@ interface LinkTableRowProps {
 }
 
 export default function LinkTableRow({ shortlink = undefined, setShowDelete = (_) => {}, setTagToDelete = (_) => {}, initShowEditDialog }: LinkTableRowProps) {
+  const { toast } = useToast()
   if(shortlink) {
+    const copyClipboard = () => {
+      navigator.clipboard.writeText(`${process.env.BASE_URL}/${shortlink.tag}`);
+      toast({
+        description: `Copied "${process.env.BASE_URL}/${shortlink.tag}" to clipboard`,
+      });
+    }
     return (
       <TableRow>
         <TableCell className="text-left font-medium">{shortlink.tag}</TableCell>
@@ -24,6 +32,7 @@ export default function LinkTableRow({ shortlink = undefined, setShowDelete = (_
           </Link>
         </TableCell>
         <TableCell className="text-right">
+          <Button variant='link' className="text-white hover:text-green-400" onClick={() => copyClipboard()}><FaCopy /></Button>
           <Button variant='link' className="text-white hover:text-yellow-400" onClick={() => initShowEditDialog(shortlink)}><FaPencilAlt /></Button>
           <Button variant='link' className="text-white hover:text-red-400"><FaRegTrashAlt onClick={() => { setShowDelete(true); setTagToDelete(shortlink.tag); }} /></Button>
         </TableCell>
